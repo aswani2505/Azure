e2ec043a88bd7ae8e2c8ae21d0d42eb6022bb322
Aniket Wani <aniketw@cloud9infosystems.com>
2018-09-17 16:33:04 -0500

    Decrypt and encrypt a Windows VM

--- Deploy Simple Windows VM.ps1
+++ Deploy Simple Windows VM.ps1
@@ -2,4 +2,25 @@
 
 #New-AzureRmResourceGroup -Name WinRG
 New-AzureRmResourceGroupDeployment -Name WindowsVMDeployment -ResourceGroupName WinRG `
-  -TemplateUri https://raw.githubusercontent.com/aswani2505/Azure/master/101-Simple-Windows-VM
+  -TemplateUri https://raw.githubusercontent.com/aswani2505/Azure/master/Decrypt-Windows-VM
+
+
+
+$rgName = "WinRG"
+$keyVaultName = "WinKeyVault"
+$app = Get-AzureRmADApplication -DisplayName "WinKVApp"
+
+# Encrypt the VM
+$keyVault = Get-AzureRmKeyVault -VaultName $keyVaultName -ResourceGroupName $rgName;
+$diskEncryptionKeyVaultUrl = $keyVault.VaultUri;
+$keyVaultResourceId = $keyVault.ResourceId;
+$keyEncryptionKeyUrl = (Get-AzureKeyVaultKey -VaultName $keyVaultName -Name WinPRkey).Key.kid;
+
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $rgName `
+    -VMName "SimpleWinVM" `
+    -AadClientID $app.ApplicationId `
+    -AadClientSecret (New-Object PSCredential "user",$securePassword).GetNetworkCredential().Password `
+    -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl `
+    -DiskEncryptionKeyVaultId $keyVaultResourceId `
+    -KeyEncryptionKeyUrl $keyEncryptionKeyUrl `
+    -KeyEncryptionKeyVaultId $keyVaultResourceId
