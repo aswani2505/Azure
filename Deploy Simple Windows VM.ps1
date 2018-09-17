a87098ac121b612b35010d34a17dc9e86582819c
Aniket Wani <aniketw@cloud9infosystems.com>
2018-09-17 16:12:00 -0500

    Decrypt Windows VM

---
+++ Deploy Simple Windows VM.ps1
@@ -0,0 +1,4 @@
+﻿Connect-AzureRmAccount
+
+New-AzureRmResourceGroupDeployment -Name WindowsVMDeployment -ResourceGroupName WinRG `
+  -TemplateUri https://raw.githubusercontent.com/aswani2505/Azure/master/101-Simple-Windows-VM
