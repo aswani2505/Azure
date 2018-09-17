0b21fa1d1f54cad329613771e9fb94b5a71d9528
Aniket Wani <aniketw@cloud9infosystems.com>
2018-09-17 16:13:01 -0500

    Powershell

--- Deploy Simple Windows VM.ps1
+++ Deploy Simple Windows VM.ps1
@@ -1,4 +1,5 @@
 ﻿Connect-AzureRmAccount
 
+#New-AzureRmResourceGroup -Name WinRG
 New-AzureRmResourceGroupDeployment -Name WindowsVMDeployment -ResourceGroupName WinRG `
   -TemplateUri https://raw.githubusercontent.com/aswani2505/Azure/master/101-Simple-Windows-VM
