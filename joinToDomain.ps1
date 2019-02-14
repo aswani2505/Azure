ceb9b1df88fa7264d7a9645db71ba7c6842b0c20
Aniket Wani <aswani@asu.edu>
2019-02-13 20:48:18 -0600

    Add files via upload

---
+++ joinToDomain.ps1
@@ -0,0 +1,5 @@
+﻿$domain = "aniket.test"
+$password = "Deme@nor250593" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\aniketw" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Add-Computer -DomainName $domain -Credential $credential -Restart
