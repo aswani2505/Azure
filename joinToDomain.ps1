d3b13d95f107c4379782da92a837bcd2d8ac50ac
Aniket Wani <aswani@asu.edu>
2019-03-19 11:11:43 -0500

    upersvmss

--- joinToDomain.ps1
+++ joinToDomain.ps1
@@ -1,5 +1,5 @@
-﻿$domain = "aniket.test"
-$password = "Deme@nor250593" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\aniketw" 
+﻿$domain = "amctec.dmz"
+$password = "Webster2024!" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\amcadm" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential -Restart
