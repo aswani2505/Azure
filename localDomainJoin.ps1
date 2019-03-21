1415f6117255ac6a4e91ff5bfe86ed59dda01df3
Aniket Wani <aswani@asu.edu>
2019-03-21 15:14:35 -0500

    localDomainJoin

---
+++ localDomainJoin.ps1
@@ -0,0 +1,5 @@
+﻿$domain = "amctec.local"
+$password = "YP#=2qkJ" | ConvertTo-SecureString -asPlainText -Force
+$username = "$domain\aniketw" 
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
+Add-Computer -DomainName $domain -Credential $credential -Restart
