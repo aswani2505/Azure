1ff468de36755ca33c1a338e48cf7746b34a8413
Aniket Wani <aswani@asu.edu>
2019-04-25 14:53:45 -0500

    Delete joinToDomain.ps1

--- joinToDomain.ps1
+++
@@ -1,5 +0,0 @@
-﻿$domain = "amctec.dmz"
-$password = "Webster2024!" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\amcadm" 
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Add-Computer -DomainName $domain -Credential $credential -Restart
