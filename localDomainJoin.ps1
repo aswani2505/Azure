660164a0b28ed728484ec9778519f8dd8e045feb
Aniket Wani <aswani@asu.edu>
2019-04-25 14:53:51 -0500

    Delete localDomainJoin.ps1

--- localDomainJoin.ps1
+++
@@ -1,5 +0,0 @@
-$domain = "amctec.local"
-$password = "YP#=2qkJ" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\aniketw" 
-$credential = New-Object System.Management.Automation.PSCredential($username,$password)
-Add-Computer -DomainName $domain -Credential $credential -Restart
